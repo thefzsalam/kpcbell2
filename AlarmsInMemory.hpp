80e7ec2a743c4dc0a0859c7dba938f5f8151ffc5
Farzeen <farseenabdulsalam@gmail.com>
2018-03-04 13:02:17 +0530

    Renamed for clarity: Alarm -> Alarm24h

--- AlarmsInMemory.hpp
+++ AlarmsInMemory.hpp
@@ -1,12 +1,12 @@
 #include <cstdint>
-#include "Alarm.hpp"
+#include "Alarm24h.hpp"
 #include "Iterator.hpp"
 
 #define ALARM_STORE_MAX_NO_OF_ALARMS 100
 
 class AlarmsInMemory {
 private:
-    Alarm storage[ALARM_STORE_MAX_NO_OF_ALARMS];
+    Alarm24h storage[ALARM_STORE_MAX_NO_OF_ALARMS];
     uint8_t storage_end_index = 0;
 
 public:
@@ -16,10 +16,10 @@ public:
         const T *const entry;
     };
 
-    void add_alarm(Alarm alarm);
-    void replace_alarm(uint8_t index, Alarm alarm);
+    void add_alarm(Alarm24h alarm);
+    void replace_alarm(uint8_t index, Alarm24h alarm);
     void delete_alarm(uint8_t index);
     void delete_all();
 
-    Iterator<StoreEntry<Alarm>> iterator();
+    Iterator<StoreEntry<Alarm24h>> iterator();
 };
--- AlarmsRomStorage.hpp
+++ AlarmsRomStorage.hpp
@@ -1,10 +1,8 @@
-#include "Alarm.hpp"
+#include "Alarm24h.hpp"
 #include "Iterator.hpp"
 
 class AlarmsRomStorage {
-
 public:
-    void store_all_to_rom(Iterator<Alarm> alarms);
-    Iterator<Alarm> load_all_from_rom();
-
+    void store_all_to_rom(Iterator<Alarm24h> alarms);
+    Iterator<Alarm24h> load_all_from_rom();
 };
