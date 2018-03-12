ae795e07b1005f46cc260195803da198f23ca708
Farzeen <farseenabdulsalam@gmail.com>
2018-03-12 06:30:35 +0530

    Added comments to clarify iterator() method. Moved private: section to bottom. Abstracted Alarms24h storage[]; to StorageCell storage[];

--- AlarmsInMemory.hpp
+++ AlarmsInMemory.hpp
@@ -5,13 +5,9 @@
 #define ALARM_STORE_MAX_NO_OF_ALARMS 100
 
 class AlarmsInMemory {
-private:
-    Alarm24h storage[ALARM_STORE_MAX_NO_OF_ALARMS];
-    uint8_t storage_end_index = 0;
-
 public:
     template <typename T>
-    class StoreEntry {
+    struct StoreEntry {
         const uint8_t index;
         const T *const entry;
     };
@@ -21,5 +17,20 @@ public:
     void delete_alarm(uint8_t index);
     void delete_all();
 
+    /* Note:
+        AlarmsInMemory may contain logic to reduce 
+        fragmentation and reuse unused memory.
+        StoreEntry.index may not be contiguous.
+    */
     Iterator<StoreEntry<Alarm24h>> iterator();
+
+
+
+private:
+    struct StorageCell;
+    StorageCell storage[ALARM_STORE_MAX_NO_OF_ALARMS];
+    uint8_t storage_end_index = 0;
+
+
+
 };
